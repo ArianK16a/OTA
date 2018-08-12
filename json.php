6a3b8c2498dea4280f86b4ae4b4e7a2ca076ef1c
LMMFHHO <39061882+LMMFHHO@users.noreply.github.com>
2018-08-12 23:07:17 +0200

    Update json.php

--- json.php
+++ json.php
@@ -1,6 +1,1 @@
-{
-".\/sagit":
-    [
-        {"filename":".\/sagit\/omni-8.1.0-20180812-sagit-treskmod.zip","timestamp":1534105624},
-    ]
-}
+{"0":".\/oneplus5":[{"filename":".\/sagit\/omni-8.1.0-20180812-sagit-treskmod.zip","timestamp":1534105624}]}
